9fcaa51575da4f7eff91672cfdf8cca64572751c
Payalbh20 <117173906+Payalbh20@users.noreply.github.com>
2023-03-21 09:35:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 function major(arr)
 {
   //your code here
+	const nums = {};
+const result = majorityElement(nums);
+console.log(result); // Output: 2
+
+function majorityElement(nums) {
+  let count = 0;
+  let candidate = null;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (count === 0) {
+      candidate = nums[i];
+    }
+
+    count += nums[i] === candidate ? 1 : -1;
+  }
+
+  return candidate;
+}
    let reqF = Math.floor(arr.length/ 2);
         let freq = {};
 
