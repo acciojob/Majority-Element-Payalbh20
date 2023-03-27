a57a56b12661da39d30534866f2ce6475583fa02
Payalbh20 <117173906+Payalbh20@users.noreply.github.com>
2023-03-27 09:55:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,31 @@
-function majority(){
-	let max =0, count=0;
-	for(let i=0; i<num.length; i++){
-		if(count ==0)max =num[i];//2,
-		if(nums[i]== max) count ++; //1
-		else count --;
-	}
-	//return max;
-	console.log(max);
+function majorityElement(arr) {
+  let count = 1;
+  let majority = arr[0];
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] === majority) {
+      count++;
+    } else {
+      count--;
+      if (count === 0) {
+        majority = arr[i];
+        count = 1;
+      }
+    }
+  }
+
+  // Count the occurrences of majority element
+  count = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === majority) {
+      count++;
+    }
+  }
+
+  // Check if majority element appears more than floor(n/2) times
+  if (count > Math.floor(arr.length / 2)) {
+    return majority;
+  } else {
+    return -1;
+  }
 }
-majority();
