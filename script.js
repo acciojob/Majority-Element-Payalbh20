8d56d2766f4f7adf1b3ba49644ea97893d7dec93
Payalbh20 <117173906+Payalbh20@users.noreply.github.com>
2023-03-27 09:58:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,31 +1,19 @@
-function majorityElement(arr) {
-  let count = 1;
-  let majority = arr[0];
-
-  for (let i = 1; i < arr.length; i++) {
-    if (arr[i] === majority) {
-      count++;
-    } else {
-      count--;
-      if (count === 0) {
-        majority = arr[i];
-        count = 1;
-      }
-    }
-  }
-
-  // Count the occurrences of majority element
-  count = 0;
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] === majority) {
-      count++;
-    }
-  }
-
-  // Check if majority element appears more than floor(n/2) times
-  if (count > Math.floor(arr.length / 2)) {
-    return majority;
-  } else {
-    return -1;
-  }
-}
+function majorityElementChecker(arr) 
+ { 
+   let map = new Map(); 
+   for(let i = 0; i < arr.length; i++) { 
+        if (map.has(arr[i])) { 
+        let count = map.get(arr[i]) +1; 
+         if (count > arr.length /2) { 
+               document.write("Majority found :- " + arr[i]); 
+            return; 
+     } else 
+            map.set(arr[i], count); 
+      } 
+         else 
+             map.set(arr[i],1); 
+         } 
+         document.write(" No Majority element"); 
+ } 
+ let arr = [2,1,2]; 
+ majorityElementChecker(arr);
